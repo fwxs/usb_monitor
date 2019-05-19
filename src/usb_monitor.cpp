05682ee915e3820df79748e2ffae5ab36258673a
fwxks <mrpcmn@protonmail.com>
2019-05-18 23:59:20 -0300

    Offline function added and change of structure

--- src/usb_monitor.cpp
+++ src/usb_monitor.cpp
@@ -3,29 +3,70 @@
 /*
  Code based on http://www.signal11.us/oss/udev/
 */
-static std::unique_ptr<Udev> u;
-static std::map<std::string, std::map<std::string, std::string>> devices;
+typedef class std::unique_ptr<UsbMonitor::UsbInfo> usb_info_ptr;
+
+static std::vector<std::string> pending_devices;
+static std::map<std::string, usb_info_ptr> devices;
+static std::unique_ptr<Utils::Socket> socket_ptr;
+
 static std::string server_ip;
 static unsigned short server_port;
 
-void UsbMonitor::init(const std::string &r_ip, const unsigned short r_port)
+static std::string get_pending_devices(const std::string &last_device)
 {
-    server_ip = r_ip;
-    server_port = r_port;
+    std::string pending = "[";
 
-    // Initialize udev object.
-    u = std::make_unique<Udev>();
+    std::for_each(std::begin(pending_devices), std::end(pending_devices), [&] (std::string &dev_properties){
+        pending.append(dev_properties + ',');
+    });
+
+    pending += last_device + "]";
+
+    return pending;
 }
 
-void UsbMonitor::start()
+static void report(const std::string& dev_properties){
+    // Report device disconnection to remote server.
+    if(socket_ptr->establish_connection())
+    {
+        if(pending_devices.empty())
+        {
+            socket_ptr->send_data(dev_properties);
+        }
+        else
+        {
+            socket_ptr->send_data(get_pending_devices(dev_properties));
+            pending_devices.clear();
+        }
+    }
+    else
+    {
+        socket_ptr = std::make_unique<Utils::Socket>(server_ip, server_port);
+        socket_ptr->establish_connection();
+
+        pending_devices.push_back(dev_properties);
+
+        std::cout << "[*] Saving device info." << '\n';
+        std::cout << "[+] " << pending_devices.size() << " devices on queue.\n";
+    }
+}
+
+void UsbMonitor::start(const std::string &rhost, const unsigned short rport)
 {
-    /* Monitor for usb connections */
-    int fd = u->init_monitor();
+    server_ip = rhost;
+    server_port = rport;
+
+    socket_ptr = std::make_unique<Utils::Socket>(server_ip, server_port);
+    socket_ptr->establish_connection();
+
+    Udev udev = {};
+    int fd = udev.init_monitor();
+
     std::unique_ptr<udev_device_ptr> udev_device;
 
     std::cout << "[*] Waiting for usb devices." << '\n';
 
-    while(1){
+    while(true){
         fd_set fds;
         int ret;
 
@@ -41,17 +82,19 @@ void UsbMonitor::start()
         }
 
         if(ret > 0 && FD_ISSET(fd, &fds)){
-            if((udev_device = u->get_udev_device())){
+            if((udev_device = udev.get_udev_device())){
                 std::string action = udev_device_get_action(*udev_device);
 
-                if(action == "add")
+                if(action == "add") {
                     report_usb_connection(action, udev_device.get());
-                else if(action == "remove")
+                }
+                else if(action == "remove"){
                     report_usb_disconnection(action, udev_device.get());
-
+                }
             }
             udev_device_unref(*udev_device);
         }
+
         Utils::wait();
     }
 }
@@ -61,31 +104,26 @@ void UsbMonitor::report_usb_connection(const std::string &action, udev_device_pt
     std::cout << "[*] Device connected." << '\n';
     std::string sysname(udev_device_get_sysname(*udev_device));
 
-    // Connected device data.
-    devices[sysname].insert(std::make_pair("dev_path", udev_device_get_devpath(*udev_device)));
-    devices[sysname].insert(std::make_pair("dev_manufacturer", udev_device_get_sysattr_value(*udev_device, "manufacturer")));
-    devices[sysname].insert(std::make_pair("product", udev_device_get_sysattr_value(*udev_device, "product")));
-    devices[sysname].insert(std::make_pair("serial", udev_device_get_sysattr_value(*udev_device, "serial")));
-    devices[sysname].insert(std::make_pair("product_id", udev_device_get_sysattr_value(*udev_device, "idProduct")));
-    devices[sysname].insert(std::make_pair("vendor_id", udev_device_get_sysattr_value(*udev_device, "idVendor")));
-    devices[sysname].insert(std::make_pair("speed", udev_device_get_sysattr_value(*udev_device, "speed")));
-    devices[sysname].insert(std::make_pair("max_pkt_size", udev_device_get_sysattr_value(*udev_device, "bMaxPacketSize0")));
-    devices[sysname].insert(std::make_pair("max_power", udev_device_get_sysattr_value(*udev_device, "bMaxPower")));
+    devices.emplace(sysname, std::make_unique<UsbMonitor::UsbInfo>(udev_device));
+    std::string dev_properties(Utils::serialize_data(action, devices[sysname]->properties()));
 
-    // Report device connection to remote server.
-    Utils::send_data(server_ip, server_port, Utils::serialize_data(action, devices[sysname]));
+    report(dev_properties);
 }
 
 void UsbMonitor::report_usb_disconnection(const std::string &action, udev_device_ptr* udev_device)
 {
     std::cout << "[*] Device disconnected." << '\n';
-    std::string sysname(udev_device_get_sysname(*udev_device));
 
-    // Report device disconnection to remote server.
-    Utils::send_data(server_ip, server_port, Utils::serialize_data(action, devices[sysname]));
+    if(!devices.empty()){
+        std::string sysname(udev_device_get_sysname(*udev_device));
+        std::string dev_properties = Utils::serialize_data(action, devices[sysname]->properties());
 
-    // Erase disconnected device record.
-    devices.erase(sysname);
+        report(dev_properties);
+
+        // Erase disconnected device record.
+        devices.at(sysname).reset(nullptr);
+        devices.erase(sysname);
+    }
 }
 
 void UsbMonitor::signal_handler(int signum)
@@ -98,8 +136,28 @@ void UsbMonitor::signal_handler(int signum)
 void UsbMonitor::stop()
 {
     std::cout << "[*] Shutting down monitor." << '\n';
-    u.reset(nullptr);
-
     std::cout << "[*] Flushing devices map." << '\n';
     devices.clear();
+}
+
+UsbMonitor::UsbInfo::UsbInfo(const udev_device_ptr* udev_device)
+{
+    this->_properties.emplace("dev_path", udev_device_get_devpath(*udev_device));
+
+    this->_properties.emplace("manufacturer", udev_device_get_sysattr_value(*udev_device, "manufacturer"));
+    this->_properties.emplace("product", udev_device_get_sysattr_value(*udev_device, "product"));
+
+    this->_properties.emplace("serial", udev_device_get_sysattr_value(*udev_device, "serial"));
+    this->_properties.emplace("product_id", udev_device_get_sysattr_value(*udev_device, "idProduct"));
+
+    this->_properties.emplace("vendor_id", udev_device_get_sysattr_value(*udev_device, "idVendor"));
+    this->_properties.emplace("speed", udev_device_get_sysattr_value(*udev_device, "speed"));
+
+    this->_properties.emplace("max_speed", udev_device_get_sysattr_value(*udev_device, "bMaxPacketSize0"));
+    this->_properties.emplace("power", udev_device_get_sysattr_value(*udev_device, "bMaxPower"));
+}
+
+UsbMonitor::UsbInfo::~UsbInfo()
+{
+    this->_properties.clear();
 }
--- src/usb_monitor.hpp
+++ src/usb_monitor.hpp
@@ -3,25 +3,36 @@
 #include "udev.hpp"
 #include "utils.hpp"
 
+#include <algorithm>
 #include <ctime>
-#include <exception>
-#include <iostream>
 #include <libudev.h>
 #include <memory>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/select.h>
+#include <utility>
 
 
-namespace  UsbMonitor
+namespace UsbMonitor
 {
-
-    void init(const std::string &, const unsigned short);
-    void start [[noreturn]] ();
+    void start [[noreturn]] (const std::string &rhost, const unsigned short rport);
     void report_usb_connection(const std::string &, udev_device_ptr *);
     void report_usb_disconnection(const std::string &, udev_device_ptr *);
     void stop();
     void signal_handler [[noreturn]] (int);
+
+    class UsbInfo
+    {
+        private:
+            std::map<std::string, std::string> _properties;
+
+        public:
+            UsbInfo(const udev_device_ptr* udev_device);
+
+            const std::map<std::string, std::string> &properties() const { return _properties; }
+
+            virtual ~UsbInfo();
+    };
 }
 
 #endif // USB_MONITOR_HPP_INCLUDED
